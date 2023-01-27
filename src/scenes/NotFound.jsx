59603efc81ba64983625468c9e8d889b33baf816
Gunduz Mehmet <gunduzmg86@gmail.com>
2023-01-27 14:43:20 +0100

    deployment

---
+++ src/scenes/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page introuvable</h1>
+      <p>Désolé, la page que vous avez demandée n'a pas été trouvée.</p>
+    </div>
+  );
+}
+export default NotFound;
